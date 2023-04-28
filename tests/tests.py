25a663e3ad57328e4187f768668c6973e9f2e132
hannalaguilar <hanna.laguilar@gmail.com>
2023-04-29 01:17:27 +0200

    DT fit and predict and its test, works!!

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.datasets import make_classification
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_classification, load_iris
 from sklearn.metrics import accuracy_score
 import pandas as pd
 
@@ -18,33 +19,46 @@ def test_gini():
     left_class = np.array(['C+', 'C+', 'C-', 'C+', 'C-', 'C+'])
     right_class = np.array(['C-', 'C-'])
 
-    assert dec_tree._gini_index(left_class, right_class) == 1/3
+    assert dec_tree._gini_gain(left_class, right_class) == 0.5 - 1 / 3
 
 
 def test_best_split():
     # data
     df = pd.read_csv(CURRENT_PATH / 'test_data.csv')
-    X = df.iloc[:, :-1]
+    X = df.iloc[:, :-1].values
     y = df.iloc[:, -1].values
 
     dec_tree = DecisionTree()
-    best_gini, best_feature, best_subset = dec_tree._best_split(X, y)
+    best_gini, best_feature_idx, best_threshold = dec_tree._best_split(X, y, [0, 1, 2])
 
-    assert np.round(best_gini, 3) == 0.2
-    assert best_feature == 'eye_color'
-    assert best_subset == [['brown', 'green'], ['blue']]
+    assert np.round(best_gini, 3) == 0.5 - 0.2
+    assert best_feature_idx == 0
+    assert best_threshold == 'blue'
 
 
 def test_decision_tree():
-    # data
     random_number = np.random.randint(np.iinfo(np.int32).max)
-    X, y = make_classification(n_samples=1000, n_features=4,
-                               n_informative=3, n_redundant=0, n_repeated=0,
-                               n_classes=3, random_state=random_number)
+
+    # data
+    X, y = load_iris(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        random_state=random_number)
 
     # sklearn
-    clf_sklearn = DecisionTreeClassifier(random_state=0, max_depth=2)
-    clf_sklearn.fit(X, y)
+    clf_sklearn = DecisionTreeClassifier(random_state=0, max_leaf_nodes=3)
+    clf_sklearn.fit(X_train, y_train)
+    acc_test_sklearn = clf_sklearn.score(X_test, y_test)
+
+    # my algorithm
+    clf_src = DecisionTree()
+    clf_src._best_split(X_train, y_train)
+    clf_src.fit(X_train, y_train)
+    y_pred_src = clf_src.predict(X_test)
+    acc_test_src = accuracy_score(y_test, y_pred_src)
+
+    # assert if the difference is less than 10%
+    print(f'sklearn:{acc_test_sklearn}, my_algorithm: {acc_test_src}')
+    np.testing.assert_allclose(acc_test_sklearn, acc_test_src, rtol=0.1)
 
 
 def test_random_forest():
